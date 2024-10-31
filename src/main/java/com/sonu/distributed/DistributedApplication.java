a9f71f317061ae2c577df44f01de75e44b21dad2
Sonu Parekaden <sonu24143parekaden@gmail.com>
2024-10-31 10:43:31 +0530

    Testing kafka config.

--- src/main/java/com/sonu/distributed/DistributedApplication.java
+++ src/main/java/com/sonu/distributed/DistributedApplication.java
@@ -1,13 +1,44 @@
 package com.sonu.distributed;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
-@SpringBootApplication
-public class DistributedApplication {
+@Slf4j
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class DistributedApplication implements ApplicationRunner {
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	@Autowired
+	private KafkaProducer<String, String> kafkaProducer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DistributedApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+		try {
+			log.info("Sending test message");
+			kafkaTemplate.send("Hello-Kafka", "Test Message kafka template"+System.currentTimeMillis());
+			kafkaProducer.send(new ProducerRecord<>("Hello-Kafka", "Test Message kafka producer"+System.currentTimeMillis()));
+		} finally {
+			kafkaTemplate.destroy();
+			kafkaProducer.close();
+		}
+	}
+
+	@KafkaListener(topics = "Hello-Kafka", groupId = "test-consumer-group")
+	public void listen(String message) {
+		log.info("Consumed message [{}].", message);
+	}
 }
---
+++ src/main/java/com/sonu/distributed/config/KafkaProducerConfig.java
@@ -0,0 +1,38 @@
+package com.sonu.distributed.config;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+    @Bean
+    ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+    @Bean
+    KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    KafkaProducer<String, String> getKafkaProducer() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        map.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        map.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new KafkaProducer<>(map);
+    }
+}
